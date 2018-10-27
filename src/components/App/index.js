402bc8cadc77aae8cea67057f1938f89fbacc398
Kai Wu <wu.vkcyan@gmail.com>
2018-10-27 19:05:52 +0800

    update

---
+++ src/components/App/index.js
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+import logo from "../../logo.svg";
+import "./style.css";
+import Header from "../../components/Header";
+import OrderItem from "../../components/OrderItem";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>Hello word</p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
