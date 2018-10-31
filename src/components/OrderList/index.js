01c02dd804003ab794aeb764a82a41267dac328b
小五 <wu.vkcyan@gmail.com>
2018-10-31 16:24:16 +0800

    update all

--- src/components/OrderList/index.js
+++ src/components/OrderList/index.js
@@ -11,7 +11,6 @@ export default class index extends Component {
   componentDidMount() {
     fetch("/mock/order.json").then(res => {
       if (res.ok) {
-        console.log(res);
         res
           .json()
           .then(data => {
@@ -28,10 +27,31 @@ export default class index extends Component {
       <div>
         {
           this.state.data.map(e => { // 一定要return 因为这个是js 第一要加key值
-            return <OrderItem key={e.id} data={e} />
+            return <OrderItem key={e.id} data={e} onSubmit= {this.handleSumbit} />
           })
         }
       </div>
     );
+  }
+  handleSumbit = (id, comment, stars) => {
+    // 这里写的有点复杂 但是实现的比较有意思
+    // const newData =  this.state.data.map(e => {
+    //   return e.id === id  ? {
+    //     ...e, comment, stars, ifCommented: true
+    //   } : e
+    // })
+    // this.setState({data:newData})
+    
+    let data = this.state.data
+    data.map(e => {
+      if (e.id === id) {
+        e.comment = comment
+        e.stars = stars
+        e.ifCommented = true
+      }
+    })
+    this.setState({
+      data
+    })
   }
 }
